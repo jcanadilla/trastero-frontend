f817b63808e945fd7ffe8fd2bdba21b0815b20c9
Raquel <raquel.diaz@bluetab.net>
2020-04-10 12:19:24 +0200

    fixes

--- src/app/pages/categorias/categorias.module.ts
+++ src/app/pages/categorias/categorias.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { CategoriasRoutingModule } from './categorias-routing.module';
 import { CategoriasComponent } from './categorias.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { NbCardModule } from '@nebular/theme';
 
 
 @NgModule({
@@ -10,6 +12,8 @@ import { CategoriasComponent } from './categorias.component';
   imports: [
     CommonModule,
     CategoriasRoutingModule,
+    Ng2SmartTableModule,
+    NbCardModule
   ],
 })
 export class CategoriasModule { }
--- src/app/pages/miscellaneous/miscellaneous.module.ts
+++ src/app/pages/miscellaneous/miscellaneous.module.ts
@@ -4,15 +4,18 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { MiscellaneousRoutingModule } from './miscellaneous-routing.module';
 import { MiscellaneousComponent } from './miscellaneous.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { NbButtonModule, NbCardModule } from '@nebular/theme';
 
 @NgModule({
   imports: [
     ThemeModule,
     MiscellaneousRoutingModule,
+    NbButtonModule,
+    NbCardModule
   ],
   declarations: [
     MiscellaneousComponent,
-    NotFoundComponent,
+    NotFoundComponent
   ],
 })
 export class MiscellaneousModule { }

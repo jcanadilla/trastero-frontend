4424313d1611d9e8af1857232ad4864818f29ea3
Raquel <raquel.diaz@bluetab.net>
2020-04-10 12:04:19 +0200

    Eliminados imports innecesarios

--- src/app/pages/miscellaneous/miscellaneous.module.ts
+++ src/app/pages/miscellaneous/miscellaneous.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { NbButtonModule, NbCardModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { MiscellaneousRoutingModule } from './miscellaneous-routing.module';
@@ -9,8 +8,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
   imports: [
     ThemeModule,
-    NbCardModule,
-    NbButtonModule,
     MiscellaneousRoutingModule,
   ],
   declarations: [
